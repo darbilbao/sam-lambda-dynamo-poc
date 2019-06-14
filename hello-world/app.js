4b255df405edd2e23633a495ca8344ba7c27ccc2
Irune Prado Alberdi <ipradoal@everis.com>
2019-06-14 11:38:34 +0200

    add. log verbosity and table test

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,15 +1,30 @@
 const DB = require('./aws-dynamo');
 const Dynamo = new DB();
 
+
+let CatCounterTableName = 'CatBreedTableDefault';
+
 exports.lambdaHandler = async (event) => {
-    console.log(event);
+    console.log('handler | in with event value:' + JSON.stringify(event));		
+	console.log('handle | process env ' + JSON.stringify(process.env));
+	
+	console.log('handle | process.env.CAT_TABLENAME: ' + process.env.CAT_TABLENAME);
+	console.log('handle | process.env.TABLE_NAME: ' + process.env.TABLE_NAME);
+	
+	CatCounterTableName = process.env.CAT_TABLENAME;
+	if (CatCounterTableName == undefined){
+		console.error('handler | error | no env TABLE_NAME');
+		CatCounterTableName = 'CatBreedTable';
+	}
+	console.log('handler | CatCounterTableName: ' + CatCounterTableName);
+	
     if (event.httpMethod === 'PUT') {
-        let response = await putCat(event)
+        let response = await putCat(CatCounterTableName, event);
         return done(response);
 		
     } else if (event.httpMethod === 'GET') {
-        //let response = await getCat(event);
-		let response = await getCatAll(event);
+        // let response = await getCatSimple(event);
+		let response = await getCatAll(CatCounterTableName, event);
         return done(response);
     }
 };
@@ -34,7 +49,28 @@ const cats = {
 }
 
 
-const putCat = async event => {
+const getCatSimple = event => {
+	return cats;
+}
+
+const getCat = (CatCounterTableName, event) => {
+    //let breed = event.pathParameters.breed;
+    //return cats[breed];
+	return cats;
+}
+
+const getCatAll = async (CatCounterTableName, event) => {
+    let breed = '*'; // event.pathParameters.breed;
+    let data = await Dynamo.scan('breedId', breed, CatCounterTableName);
+    let result = data.Items.sort((a,b) => b.count - a.count);
+    result = result.map(({count, ID, breed})=> { return {count, ID, breed}});
+    return data;
+}
+
+
+
+
+const putCat = async (CatCounterTableName, event) => {
 	console.log(event.body);
     let params = JSON.parse(event.body);
 	let breed = params.breed;
@@ -42,20 +78,5 @@ const putCat = async event => {
 	
 	// let ID = event.pathParameters["breed"];
     // console.log(ID);	
-    return Dynamo.increment(breed, 'CatBreedTable')
-}
-
-
-const getCat = event => {
-    //let genre = event.pathParameters.genre;
-    //return cats[genre];
-	return cats;
-}
-
-const getCatAll = async event => {
-    let genre = 'SIA'; // event.pathParameters.genre;
-    let data = await Dynamo.scan('breedId', genre, 'CatBreedTable');
-    let result = data.Items.sort((a,b) => b.count - a.count);
-    result = result.map(({count, ID, genre})=> { return {count, ID, genre}});
-    return data;
+    return Dynamo.increment(breed, CatCounterTableName)
 }
